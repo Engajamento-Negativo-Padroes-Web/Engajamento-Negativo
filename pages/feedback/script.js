b6639210d87433c3edd2f2dcd16cf4a1558334c8
HappyPotatomato <artur.beey@gmail.com>
2025-10-18 17:10:00 -0300

    feedback page

--- pages/feedback/script.js
+++ pages/feedback/script.js
@@ -2,9 +2,9 @@ function enviarFeedback() {
     const user_subject = document.getElementById("feedback-subject").value;
     const subject = "Feedback Engajamento Negativo - " + user_subject;
     const body = document.getElementById("feedback-body").value;
-    const emails = "engajamentonegativo@gmail.com";
+    const emails = "example@example.com, feedback@example.com";
 
-    if (!user_subject || !body) {
+    if (!subject || !body) {
         alert("Por favor, preencha todos os campos.");
         return;
     }
