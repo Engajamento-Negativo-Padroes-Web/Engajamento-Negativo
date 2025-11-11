e9e1e5efccc9edb970349e1b26d09771318c4eb3
Thiago S. Marcelino <thiagodsmarcelino@gmail.com>
2025-11-11 08:37:18 -0300

    feat: restructure about us page and update links across the site

--- pages/feedback/script.js
+++ pages/feedback/script.js
@@ -1,15 +1,16 @@
 function enviarFeedback() {
-    const user_subject = document.getElementById("feedback-subject").value;
-    const subject = "Feedback Engajamento Negativo - " + user_subject;
-    const body = document.getElementById("feedback-body").value;
-    const emails = "engajamentonegativo@gmail.com";
+  const user_subject = document.getElementById("feedback-subject").value;
+  const subject = "Feedback Engajamento Negativo - " + user_subject;
+  const body = document.getElementById("feedback-body").value;
+  const emails = "engajamentonegativo@gmail.com";
 
-    if (!user_subject || !body) {
-        alert("Por favor, preencha todos os campos.");
-        return;
-    }
-    else {
-        const mailtoLink = `mailto:${emails}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-        window.open(mailtoLink);
-    }
+  if (!user_subject || !body) {
+    alert("Por favor, preencha todos os campos.");
+    return;
+  } else {
+    const mailtoLink = `mailto:${emails}?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(body)}`;
+    window.open(mailtoLink);
+  }
 }
